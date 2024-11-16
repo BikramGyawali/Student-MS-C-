7908fb781651b56d01c8b21e8c46b27a13fb9e55
Bikram Gyawali <gyawalibikram0@gmail.com>
2024-11-16 15:18:42 +0545

    Complete Delete Function

--- Student management system.c
+++ Student management system.c
@@ -3,16 +3,19 @@
 #include<stdio.h>
 #include<string.h>
 #include<stdlib.h>
-
 struct student{
 	int id,p,sem;
 	char fname[50],a[50],f[50],lname[50] ;
 }s[100];
-
  void space(){
-	printf("\n\n\t\t\t");
+	printf("\n\n\t\t\t\t\t\t\t");
 }
-void Added_Data();
+void space1(){
+	printf("\n\n\t\t\t\t");
+}
+void Added_Data(); //calling this function at last of the code
+
+//Function for seeDetail
 void seeDetail(){
 	FILE *fp;
 	fp=fopen("student.txt","r");
@@ -25,20 +28,20 @@ void seeDetail(){
 	space(); printf("Semester : %d",s1.sem);
 	space(); printf("Address : %s",s1.a);
 	space(); printf("Phone Number : %d",s1.p);
+	space();
 }
 	fclose(fp);
 }
+
+
 //Starting Search Function
-
-
-
 void search() {
     char opt[50], sname[50];
     int rfound = 0, sid;
     struct student s1;
     FILE *fp;
 
-    space(); printf("Search By Id or Name): ");
+    space(); printf("Search By Id or Name : ");
     scanf("%s", opt);
 
     fp = fopen("student.txt", "r");
@@ -79,6 +82,7 @@ void search() {
                 space(); printf("Semester : %d", s1.sem);
                 space(); printf("Address : %s", s1.a);
                 space(); printf("Phone Number : %d", s1.p);
+                space();
                 rfound = 1;
             }
         }
@@ -88,23 +92,31 @@ void search() {
         }
     } else {
         space(); printf("Invalid search option! Please choose 'id' or 'name'.");
+        space();
     }
 
     fclose(fp);
 }
 
+//Stating Main Section
 main(){
+	FILE *fp;
+	 FILE *nfp;
+ 	int did,found=0;
+ 	char dname[50],opt[50];
+ 	struct student s1;
 	int choice ;
 			//Task To be Perform
 space();	printf(" Welcome to Sarswati Multiple Campus\n\n ");
 do{
- printf("----------------------------------------------------------------------------------------");
+	loop1:
+ space1();printf("----------------------------------------------------------------------------------------");
 space();	printf("1.Add");
 space();	printf("2. See All Student Details ");
 space();	printf("3. Search Students");
-space(); printf("4. Update ")
+space(); printf("4. Delete  ");
 
-space();	printf("4.Exist");
+space();	printf("5.Exist");
 	
 		//For Geeting the choise Form User
 		
@@ -113,6 +125,8 @@ space();	printf("Enter the Number to perform Task:\t");
 	
 	
 	switch(choice){
+		
+		//Starting the Cases to call the function
 		case 1:
 		Added_Data();
 			break;
@@ -123,20 +137,92 @@ space();	printf("Enter the Number to perform Task:\t");
 				search();
 			break;
 			case 4:
-				printf("Existing...............");
-				printf("\n\nMay this help you to keep records properly");
+
+// Starting Delete Function
+ 
+  fp = fopen("student.txt", "r");
+    if (fp == NULL) {
+        space(); printf("Error opening file 'student.txt'!");
+        return;
+    }
+
+    nfp = fopen("newFile.txt", "w");
+    if (nfp == NULL) {
+        space(); printf("Error creating temporary file!");
+        fclose(fp);
+        return;
+    }
+
+    space(); printf("Want to delete data by 'Id' or 'Name': ");
+    scanf("%s", opt);
+
+    // Delete data by id
+    if (strcmp(opt, "id") == 0 || strcmp(opt, "Id") == 0) {
+        space(); printf("Enter the Roll-No: ");
+        scanf("%d", &did);
+        while (fread(&s1, sizeof(struct student), 1, fp) == 1) {
+            if (s1.id == did) {
+                found = 1;
+                space(); printf("Successfully deleted the data of Roll-No %d", did);
+            } else {
+                fwrite(&s1, sizeof(struct student), 1, nfp);
+            }
+        }
+        if (!found) {
+            space(); printf("No record found for Roll-No: %d", did);
+        }
+    } 
+    // Delete data by name
+    else if (strcmp(opt, "name") == 0 || strcmp(opt, "Name") == 0) {
+        space(); printf("Enter the Name: ");
+        scanf("%s", dname);
+        while (fread(&s1, sizeof(struct student), 1, fp) == 1) {
+            if (strcmp(dname, s1.fname) == 0 || strcmp(dname, s1.lname) == 0) {
+                found = 1;
+                space(); printf("Successfully deleted the data of %s", dname);
+            } else {
+                fwrite(&s1, sizeof(struct student), 1, nfp);
+            }
+        }
+        if (!found) {
+            space(); printf("No record found for Name: %s", dname);
+        }
+    } 
+    // Invalid option
+    else {
+        space(); printf("Invalid search option! Please choose 'id' or 'name'.");
+        fclose(fp);
+        fclose(nfp);
+        remove("newFile.txt");
+        return;
+    }
+
+    fclose(fp);
+    fclose(nfp);
+    remove("student.txt"); // Correct filename
+    rename("newFile.txt", "student.txt");
+    
+    space();
+    goto loop1;
+    
+//				printf("\n\nMay this help you to keep records properly");
 			break;
+			case 5: 
+		space();	printf("Exiting...................................");
+		 space();printf("May this system hepl you .......");
+		 space();space();
+		break;
+			
 			default :
 				printf("Enter the valid Number");
 				break;
 				
 	} 
-}while(choice!=4);
+}while(choice!=5);
 	
 	
 } 
-
-   		//Function Call for adding data
+	//Function Call for adding data
  void Added_Data(){
  	FILE *fp;
  		struct student  s;
@@ -164,13 +250,13 @@ space();	printf("Enter the number of student that you want to keep record of: ")
       space();  printf("Enter Phone Number for student %d:  ", i + 1);
        scanf("%d", &s.p);
         space(); printf("Sucessfully Enter the data of %d Student",i+1);
+        space();
         fwrite(&s, sizeof(struct student),1,fp);
         
-        fclose(fp);
+      
    
  }
+   fclose(fp);
 }
-// Calling Search Function
-
 
 
